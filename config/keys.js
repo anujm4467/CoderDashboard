b8d0360ff5b53a955c1028088b4ae91f77511965
anujm4467 <anujm4467@gmail.com>
2018-11-13 22:58:44 +0530

    page1

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+/** @format */
+
+module.exports = {
+  mongoURI: "mongodb://localhost:27017/developerconnecter",
+  secrate_key: "sercrate"
+};
--- devconnect/config/index.js
+++
@@ -1,6 +0,0 @@
-/** @format */
-
-module.exports = {
-  database_link: 'mongodb://anujm4467:ANuj1234=-@ds251223.mlab.com:51223/devconnectore',
-  secrate_key: 'sercrate',
-}
